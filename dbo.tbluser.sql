e1d22ffa60cdb1f0e3ed91257ed3340eea0a46f8
mohamed mootaz ben kraiem <96307470+mootazkraiem@users.noreply.github.com>
2022-03-22 15:38:24 +0100

    Add files via upload

---
+++ dbo.tbluser.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tbluser] (
+    [username]  VARCHAR (50) NOT NULL,
+    [password]  VARCHAR (50) NULL,
+    [nom]       VARCHAR(50) NULL,
+    [prenom]    VARCHAR(50) NULL,
+    [adresse]   VARCHAR(50) NULL,
+    [telephone] VARCHAR(10)          NULL,
+    [metier]    VARCHAR(50) NULL,
+    [email]     VARCHAR(320) NULL,
+    PRIMARY KEY CLUSTERED ([username] ASC)
+);
+
